cdd4122f23949042a686fc242600338eb46aa9d8
Thanikachalam1 <thanikachalamgms10@gmail.com>
2025-04-29 11:15:19 +0530

    true focus effect added

--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import './home.css';
+import TrueFocus from "../Animations/TrueFocus";
 
 function Home(){
   return(
-    <div className="Home">
+    <div className="Home" id = "home">
       <div className="intro">
+
+      
         <h1 className="name"><span className="hi">Hi, I'm </span>Thanikachalam</h1>
-        <p className="roles">Student | Developer | Content Creator</p>
+        <TrueFocus 
+sentence="Developer Student Content-Creator"
+manualMode={false}
+blurAmount={5}
+borderColor="red"
+animationDuration={0.5}
+pauseBetweenAnimations={1}
+/>
         <p className="specialization">Specialized in developing web pages</p>
       </div>
     </div>
